576e3f92b9f4d6815e50f57a6f9309aa8adeeb70
XiaoYuanJW <2754742370@qq.com>
2022-11-20 01:56:17 +0800

    ElasticSearch RestClient

---
+++ spring-demo-elasticsearch/src/main/java/com/example/demo/SpringDemoElasticSearchApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用启动入口
+ * Created by YuanJW on 2022/11/8.
+ */
+@SpringBootApplication
+public class SpringDemoElasticSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoElasticSearchApplication.class, args);
+    }
+}
